42cbb388b3a857ce431f60cab28616b1414b0239
ymd65536 <ymd65536@gmail.com>
2021-09-07 09:09:15 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+﻿# http://localhost:9515/session
+
+import requests
+import json
+# POST先URL
+url = "http://localhost:9515/session"
+head = {'Content-Type': 'application/json; charset=utf-8'}
+payload = {'capabilities': {}}
+r = requests.post(url, headers=head, data=json.dumps(payload))
+
+print(r.text)
